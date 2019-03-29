a9fe0b67cdeedcac716fe76caf0b648a0371cd6f
weeshun <weeshun@gmail.com>
2019-03-28 17:36:31 -0700

    Saving the downloaded code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+/**
+  This CodeSandbox has been automatically generated using
+  `codesandboxer`. If you're curious how that happened, you can
+  check out our docs here: https://github.com/noviny/codesandboxer
+
+  If you experience any struggles with this sandbox, please raise an issue
+  on github. :)
+*/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './example';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+<App />,
+document.getElementById('root')
+);
